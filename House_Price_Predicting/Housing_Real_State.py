ba30aa479c6af9932570c28838ac9d3acd09556d
hellosandeep1999 <sandeepjain20178@gmail.com>
2020-05-19 10:50:17 +0530

    Added new file

--- House_Price_Predicting/Housing_Real_State.py
+++ House_Price_Predicting/Housing_Real_State.py
@@ -268,10 +268,24 @@ final_rmse = np.sqrt(final_mse)
 
 print(final_rmse)
 
+print(final_predictions,list(Y_test))
+
+prepared_data[0]
 
 
 
 
+
+#Final Using the model
+from joblib import dump, load
+import numpy as np
+model = load("housingRealstate.joblib")
+
+features = np.array([[-0.43942006,  7.12628155, -1.12165014, -0.27288841, -1.42262747,
+       -0.24023069, -555.31238772,  2.61111401, -1.0016859 , -0.5778192 ,
+       -0.97491834,  1.41164221, -0.86091034]]) 
+
+print("final model result  :  ",model.predict(features))
 
 
 
